b50f43cbb1e6976a888fbea66afb0006d112a4b6
David Ward <davidward1307@gmail.com>
2022-02-06 15:58:33 +0000

    styled Log in pop up, created signup page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-const open = document.getElementById("open");
-const login_popup = document.getElementById("login-popup");
+const open = document.getElementById("open"); 
+const login_popup = document.getElementById("login_popup");
 const close = document.getElementById("close");
 
 open.addEventListener("click", () => {
     login_popup.classList.add("show");
 });
 
-close.addEventListener("click", ()=>{
+close.addEventListener("click", () => {
     login_popup.classList.remove("show");
 });
